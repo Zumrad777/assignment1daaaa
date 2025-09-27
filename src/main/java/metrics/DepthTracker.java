e1107d8e8743f7887496654daeb0ca4b9140aae0
Zumrad777 <sherbadalovaz@gmail.com>
2025-09-28 00:42:22 +0500

    init: maven, junit5, readme

---
+++ src/main/java/metrics/DepthTracker.java
@@ -0,0 +1,26 @@
+package metrics;
+
+public class DepthTracker {
+    private int currentDepth = 0;
+    private int maxDepth = 0;
+
+    public void enter() {
+        currentDepth++;
+        if (currentDepth > maxDepth) {
+            maxDepth = currentDepth;
+        }
+    }
+
+    public void exit() {
+        currentDepth--;
+    }
+
+    public int getMaxDepth() {
+        return maxDepth;
+    }
+
+    public void reset() {
+        currentDepth = 0;
+        maxDepth = 0;
+    }
+}
